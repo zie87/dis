2d939d08fc53d3c9de50917bfe5df9a4bae8b081
Tobias Zindl <zie.develop@gmail.com>
2023-04-01 08:47:53 +0200

    :construction: add iterator support

---
+++ tests/unit_tests/iterator/forward_iterator_tests.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright © Tobias Zindl 2023
+ *
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+ *
+ */
+
+
+#include "test_framework.hpp"
+
+
+
+#include "dis/meta/type_traits/is_same.hpp"
+
+TEST(iterator_facade_forward_iterator) {}
---
+++ tests/unit_tests/iterator/input_iterator_tests.cpp
@@ -0,0 +1,18 @@
+/*
+ * Copyright © Tobias Zindl 2023
+ *
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+ *
+ */
+
+#include <dis/iterator.hpp>
+
+
+#include "test_framework.hpp"
+
+
+
+#include "dis/meta/type_traits/is_same.hpp"
+
+TEST(iterator_facade_input_iterator) {}
