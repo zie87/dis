ac82c4199331cd6e293d9ce1d57742617fd3b778
Tobias Zindl <zie.develop@gmail.com>
2023-04-28 20:47:55 +0200

    :white_check_mark: port tests to utest

--- tests/unit_tests/meta/sequence/push_front_tests.cpp
+++ tests/unit_tests/meta/sequence/push_front_tests.cpp
@@ -9,35 +9,35 @@
 #include "dis/meta/sequence.hpp"
 #include "dis/meta/type_traits/is_same.hpp"
 
-#include "test_framework.hpp"
+#include <utest.h>
 
-TEST(meta_sequence_push_front) {
+UTEST(meta_sequence, push_front) {
     {
         using seq_type  = dis::meta::sequence<>;
         using test_type = dis::meta::push_front_t<seq_type, char>;
 
-        UT_CHECK_TRUE((dis::meta::is_sequence_v<test_type>));
-        UT_CHECK_FALSE((dis::meta::empty_v<test_type>));
-        UT_CHECK_EQUAL((dis::meta::size_v<test_type>), 1U);
-        UT_CHECK_TRUE((dis::meta::is_same_v<test_type, dis::meta::sequence<char>>));
+        ASSERT_TRUE((dis::meta::is_sequence_v<test_type>));
+        ASSERT_FALSE((dis::meta::empty_v<test_type>));
+        ASSERT_EQ((dis::meta::size_v<test_type>), 1U);
+        ASSERT_TRUE((dis::meta::is_same_v<test_type, dis::meta::sequence<char>>));
     }
     {
         using seq_type  = dis::meta::sequence<char>;
         using test_type = dis::meta::push_front_t<seq_type, int>;
 
-        UT_CHECK_TRUE((dis::meta::is_sequence_v<test_type>));
-        UT_CHECK_FALSE((dis::meta::empty_v<test_type>));
-        UT_CHECK_EQUAL((dis::meta::size_v<test_type>), 2U);
-        UT_CHECK_TRUE((dis::meta::is_same_v<test_type, dis::meta::sequence<int, char>>));
+        ASSERT_TRUE((dis::meta::is_sequence_v<test_type>));
+        ASSERT_FALSE((dis::meta::empty_v<test_type>));
+        ASSERT_EQ((dis::meta::size_v<test_type>), 2U);
+        ASSERT_TRUE((dis::meta::is_same_v<test_type, dis::meta::sequence<int, char>>));
     }
     {
         using seq_type  = dis::meta::sequence<int, short, double>;
         using tmp_type  = dis::meta::push_front_t<seq_type, char>;
         using test_type = dis::meta::push_front_t<tmp_type, int>;
 
-        UT_CHECK_TRUE((dis::meta::is_sequence_v<test_type>));
-        UT_CHECK_FALSE((dis::meta::empty_v<test_type>));
-        UT_CHECK_EQUAL((dis::meta::size_v<test_type>), 5U);
-        UT_CHECK_TRUE((dis::meta::is_same_v<test_type, dis::meta::sequence<int, char, int, short, double>>));
+        ASSERT_TRUE((dis::meta::is_sequence_v<test_type>));
+        ASSERT_FALSE((dis::meta::empty_v<test_type>));
+        ASSERT_EQ((dis::meta::size_v<test_type>), 5U);
+        ASSERT_TRUE((dis::meta::is_same_v<test_type, dis::meta::sequence<int, char, int, short, double>>));
     }
 }
